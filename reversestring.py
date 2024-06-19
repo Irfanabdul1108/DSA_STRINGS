c798ff95cdab7b100e9adfa5c09b98cf0553aba1
Irfanabdul1108 <143101267+Irfanabdul1108@users.noreply.github.com>
2024-06-19 21:13:52 +0530

    Add files via upload

---
+++ reversestring.py
@@ -0,0 +1,7 @@
+# platform - Leetcode
+#link - https://leetcode.com/problems/reverse-string/
+# concept - Reversing a string
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
